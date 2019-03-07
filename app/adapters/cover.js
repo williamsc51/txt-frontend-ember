64479f7ae6d096f0fef3352728515c29aaea9515
williamsc51 <williams.a.courtney@gmail.com>
2019-03-07 12:44:34 -0500

    update img urls to remove leading /

--- app/adapters/cover.js
+++ app/adapters/cover.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
 import FormDataAdapterMixin from 'ember-cli-form-data/mixins/form-data-adapter';
+import ENV from 'txt-frontend-ember/config/environment';
 
 export default DS.JSONAPIAdapter.extend(FormDataAdapterMixin, {
-    host: 'http://localhost:3000',
+    host: ENV.host,
     namespace: 'api/v1'
 });
