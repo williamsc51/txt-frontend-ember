b8444738804deef3708edc270745eedb4b98ba8c
Courtney Williams <williams.a.courtney@gmal.com>
2019-07-17 16:46:30 -0400

    added checkout create and capture order for paypal

--- app/components/paypal-button.js
+++ app/components/paypal-button.js
@@ -3,8 +3,12 @@ import Component from '@ember/component';
 const { paypal } = window;
 
 export default Component.extend({
+
     didRender(){
         this._super(...arguments);
-        paypal.Buttons(this.payment).render('#ppb')
+        paypal.Buttons({
+          createOrder: this.createPayment,
+          onApprove: this.approvePayment
+        }).render('#ppb');
     },
 });
