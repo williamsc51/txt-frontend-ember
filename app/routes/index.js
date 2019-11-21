bbb61dbf8a3a984b8e826f0e7fe90209cc8b5451
Courtney Williams <williams.a.courtney@gmail.com>
2019-11-21 16:10:33 -0500

    Created catgory list component. add component to index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,18 +1,18 @@
 import Route from '@ember/routing/route';
-// import RSVP from 'rsvp';
+import RSVP from 'rsvp';
 import { inject as service} from '@ember/service';
 
 export default Route.extend({
 
   splashscreenService: service('ember-cordova/splash'),
 
-  // model(){
-  //   return RSVP.hash({
-  //     novels: this.store.query('book', {page: {limit: 4}, filter: {category: "novel"}}),
-  //     college: this.store.query('book',{page: {limit: 4}, filter: {category: 'college'}})
-  //   })
+  model(){
+    return RSVP.hash({
+      novels: this.store.query('book', {page: {limit: 4}, filter: {category: "novel"}}),
+      college: this.store.query('book',{page: {limit: 4}, filter: {category: 'college'}})
+    })
    
-  // },
+  },
 
   afterModel(){
     this.splashscreenService.hide();
