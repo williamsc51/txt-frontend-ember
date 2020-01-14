9402f6048246419b16a6ca831593ecf162f90e8c
Courtney Williams <williams.a.courtney@gmail.com>
2020-01-14 03:29:07 -0500

    update cart model to get book count and cart route to return cart items

--- app/models/cart.js
+++ app/models/cart.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
     user: DS.belongsTo(),
     books: DS.hasMany(),
 
-    user_id: DS.attr('number')
+    user_id: DS.attr('number'),
+    books_count: DS.attr('number')
 
 });
--- app/routes/cart.js
+++ app/routes/cart.js
@@ -1,14 +1,17 @@
 import Route from '@ember/routing/route';
-import { inject as service} from '@ember/service';
+import {inject as service} from '@ember/service';
 
 export default Route.extend({
 
     session: service(),
-    cart: service(),
+    user: service(),
     
-
     model(){
-        return this.modelFor('application');
+        if(this.session.isAuthenticated){
+            // return this.store.queryRecord('cart', {user_id: this.user.currentUser, include: 'books'})
+            return this.store.query('books_cart', {filter: {cart_id: 3}, include: 'book'})
+        }
     }
+
 
 });
