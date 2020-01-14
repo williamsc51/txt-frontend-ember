5189589ade64323879315a4bc1542eee8475c8c5
Courtney Williams <williams.a.courtney@gmail.com>
2020-01-14 03:27:33 -0500

    track current cart and cart count in application route

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,8 +1,13 @@
 import Controller from '@ember/controller';
 import { inject as service} from '@ember/service';
+import { observer } from '@ember/object';
 
 export default Controller.extend({
   
   cart: service(),
-
+  // eslint-disable-next-line ember/no-observers
+  currentCart: observer('model.id', function(){
+    this.set('cart.cartId', this.model.id)
+  }),
+    
 });
