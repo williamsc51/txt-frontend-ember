1b2a5a3ffb767599933bf0d2685763baca5718de
Courtney Williams <williams.a.courtney@gmail.com>
2020-01-14 03:22:21 -0500

    add user service to track current user

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,19 +1,19 @@
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import { inject as service} from '@ember/service';
 
 export default Route.extend(ApplicationRouteMixin, {
 
+    user: service(),
+
     model(){
         if(this.session.isAuthenticated){
-            let current_user = this.session.data.authenticated.id
-            return this.store.queryRecord('cart', {user_id: current_user, include: 'books'}).catch(() => {
+            return this.store.queryRecord('cart', {user_id: this.user.currentUser}).catch(() => {
                 let newCart = this.store.createRecord('cart', {
-                    user_id: current_user
+                    user_id: this.user.currentUser
                 });
                 newCart.save();
-            })
-
+            })        
         }
-    }
-
+    },
 });
---
+++ app/services/user.js
@@ -0,0 +1,12 @@
+import Service from '@ember/service';
+import { computed } from '@ember/object';
+import { inject as service} from '@ember/service';
+
+export default Service.extend({
+
+  session: service(),
+
+  currentUser: computed(function(){
+    return this.session.data.authenticated.id
+  })
+});
