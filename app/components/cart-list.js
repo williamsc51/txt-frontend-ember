5227cd0599cff49c49b678368141a9d951a1f86e
Courtney Williams <williams.a.courtney@gmail.com>
2020-01-14 03:31:02 -0500

    update methods to add and remove items from cart

--- app/components/cart-list.js
+++ app/components/cart-list.js
@@ -1,4 +1,13 @@
 import Component from '@ember/component';
+import { inject as service} from '@ember/service';
 
 export default Component.extend({
+
+  cart: service(),
+
+  actions: {
+    removeItem(bookId){
+      this.cart.remove(bookId)
+    }
+  }
 });
--- app/services/cart.js
+++ app/services/cart.js
@@ -1,75 +1,55 @@
 import Service from '@ember/service';
 import { computed } from '@ember/object';
 import { inject as service} from '@ember/service';
-import { storageFor } from 'ember-local-storage';
 
 export default Service.extend({
 
     store: service(),
     session: service(),
+    cartId: null,
 
-
-    bookIds: storageFor('cart'),
-    
-    bookCount: computed('bookIds', 'count', function(){
-      if(this.session.isAuthenticated){
-        let bk = this.store.peekAll('book')
-         return bk.length
-      } else { return this.bookIds.length}
+    emptyCart: computed('total', function(){
+      if(this.total === 0){
+        return true
+      }
+      else{
+        return false
+      }
     }),
 
-    books: computed('bookIds.[]', function(){
+    async add(bookId){
+        let addBook = this.store.createRecord('books_cart', {book_id: bookId, cart_id: this.cartId})
+        await addBook.save()
+        this.getCart()
+    },
 
-      if(this.get('bookIds.length') == 0){
-          return []
-      } else{
-            return this.store.query('book', { filter: {id: this.get('bookIds.content')}} );
-          } 
-        
-      }),
+    getCart(){
+      let current_user = this.session.data.authenticated.id
+      return this.store.queryRecord('cart', {user_id: current_user})
+    },
 
-      emptyCart: computed('total', function(){
-
-        if(this.total === 0){
-          return true
-        }
-        else{
-          return false
-        }
-      }),
-
-       add(bookId){
-        if(this.session.isAuthenticated){
-          let current_user = this.session.data.authenticated.id
-          return this.store.createRecord('cart', {user_id: current_user, include: 'books'}).catch(() => {
-              let newCart = this.store.createRecord('cart', {
-                  user_id: current_user
-              });
-              newCart.save();
-          })
-        }else{
-            this.bookIds.addObject(bookId);
-          }
-      },
-
-      remove(bookId){
-         this.bookIds.removeObject(bookId); 
-      },
-
-      clear(){
-        this.get('bookIds').clear()
-      },
-
-      bookPrices: computed.mapBy('books', 'price'),
-
-      total: computed.sum('bookPrices'),
-
-      roundedTotal: computed('total', function(){
-        return this.get('total').toFixed(2)
-      }),
-
-      
-      centsTotal: computed('total', function(){
-        return this.get('total') * 100;
+    remove(bookId){
+      this.store.query('books_cart', {filter: {book_id: bookId, cart_id: this.cartId}}, {backgroundReload: false}).then(function(book){
+        book.forEach(function(b){
+          b.destroyRecord();
+        })
       })
+    },
+
+    clear(){
+      this.get('bookIds').clear()
+    },
+
+    bookPrices: computed.mapBy('books', 'price'),
+
+    total: computed.sum('bookPrices'),
+
+    roundedTotal: computed('total', function(){
+      return this.get('total').toFixed(2)
+    }),
+
+    
+    centsTotal: computed('total', function(){
+      return this.get('total') * 100;
+    })
 });
